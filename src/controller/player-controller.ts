632b1024f2bdc8b5522459706a2458786575c165
Teo Hao Yu <thstoneyes@gmail.com>
2026-07-10 21:54:37 +0800

    Refactor to use async/await

--- src/controller/player-controller.ts
+++ src/controller/player-controller.ts
@@ -2,31 +2,31 @@ import { type Request, type Response } from 'express';
 import { pool } from '../db/db.ts';
 
 const playerController = {
-  getAllPlayers: (req: Request, res: Response) => {
-    pool.query('SELECT * FROM players', (err, result) => {
-      if (err) {
-        console.error('Error executing query', err.stack);
-        res.status(500).send('Internal Server Error');
-      } else {
-        res.json(result.rows);
-      }
-    });
+  getAllPlayers: async (req: Request, res: Response) => {
+    try {
+      const result = await pool.query('SELECT * FROM players');
+      res.json(result.rows);
+    } catch (err) {
+      console.error('Error executing query', (err as Error).stack);
+      res.status(500).send('Internal Server Error');
+    }
   },
 
-  getPlayerById: (req: Request, res: Response) => {
+  getPlayerById: async (req: Request, res: Response) => {
     const { id } = req.params;
-    pool.query('SELECT * FROM players WHERE pid = $1', [id], (err, result) => {
-      if (err) {
-        console.error('Error executing query', err.stack);
-        res.status(500).send('Internal Server Error');
+    try {
+      const result = await pool.query('SELECT * FROM players WHERE pid = $1', [
+        id,
+      ]);
+      if (result.rows.length === 0) {
+        res.status(404).send('Player not found');
       } else {
-        if (result.rows.length === 0) {
-          res.status(404).send('Player not found');
-        } else {
-          res.json(result.rows[0]);
-        }
+        res.json(result.rows[0]);
       }
-    });
+    } catch (err) {
+      console.error('Error executing query', (err as Error).stack);
+      res.status(500).send('Internal Server Error');
+    }
   },
 };
 
