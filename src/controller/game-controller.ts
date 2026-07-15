f8def4ef5e9c91e28e2d2cf63233bea8fcbcab96
Teo Hao Yu <thstoneyes@gmail.com>
2026-07-15 23:43:06 +0800

    Add endpoint to fetch ruleset info by game ID

--- src/controller/game-controller.ts
+++ src/controller/game-controller.ts
@@ -1,10 +1,28 @@
 import { db } from '#db/index.js';
-import { gameResult, playerInfo } from '#drizzle/schema.js';
+import {
+  gameInfo,
+  gameResult,
+  playerInfo,
+  rulesetInfo,
+} from '#drizzle/schema.js';
 import { asc, eq, getColumns, type InferSelectModel } from 'drizzle-orm';
 import { type Request, type Response } from 'express';
 
 type PlayerInfo = InferSelectModel<typeof playerInfo>;
 type GameResult = InferSelectModel<typeof gameResult>;
+type RulesetInfo = {
+  initialValue: number;
+  aka: string;
+  umaP1: number;
+  umaP2: number;
+  umaP3: number;
+  umaP4: number;
+  oka: number;
+  chomboValue: number;
+  chomboOption: string;
+  kiriageMangan: boolean;
+  multipleRon: boolean;
+};
 
 const gameController = {
   async getPlayersByGameId(req: Request, res: Response) {
@@ -32,6 +50,39 @@ const gameController = {
       res.status(200).json(players);
     } catch (error) {
       console.error('Error fetching players by game ID:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+
+  async getRulesetInfoByGameId(req: Request, res: Response) {
+    const gameId: number = Number(req.params.id);
+
+    try {
+      const rulesetInfoResult: RulesetInfo | undefined = await db
+        .select({
+          initialValue: rulesetInfo.initialValue,
+          aka: rulesetInfo.aka,
+          umaP1: rulesetInfo.umaP1,
+          umaP2: rulesetInfo.umaP2,
+          umaP3: rulesetInfo.umaP3,
+          umaP4: rulesetInfo.umaP4,
+          oka: rulesetInfo.oka,
+          chomboValue: rulesetInfo.chomboValue,
+          chomboOption: rulesetInfo.chomboOption,
+          kiriageMangan: rulesetInfo.kiriageMangan,
+          multipleRon: rulesetInfo.multipleRon,
+        })
+        .from(rulesetInfo)
+        .innerJoin(gameInfo, eq(rulesetInfo.rulesetId, gameInfo.rulesetId))
+        .where(eq(gameInfo.gameId, gameId))
+        .then((results) => results[0]);
+
+      if (!rulesetInfoResult) {
+        return res.status(404).json({ error: 'Ruleset info not found' });
+      }
+
+      res.json(rulesetInfoResult);
+    } catch (error) {
       res.status(500).json({ error: 'Internal server error' });
     }
   },
--- src/routes/game-routes.ts
+++ src/routes/game-routes.ts
@@ -1,8 +1,9 @@
-import express, { type Router } from 'express';
 import gameController from '#controller/game-controller.js';
+import express, { type Router } from 'express';
 
 const router: Router = express.Router();
 
 router.get('/:id/players', gameController.getPlayersByGameId);
+router.get('/:id/ruleset', gameController.getRulesetInfoByGameId);
 
 export default router;
